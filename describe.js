a5d2ac2206b222743332a390e8eb5b2019fbaed2
airportyh <airportyh@Air.local>
2010-04-25 23:38:29 -0400

    fixes

--- describe.js
+++ describe.js
@@ -1,3 +1,25 @@
+/*
+Copyright (c) 2010, Toby Ho
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
 describe = function(name){
 	function Spec(name){
 		this.name = name;
@@ -65,7 +87,7 @@ describe.runSpec = function(spec, debug){
 	if (spec.afterAll) spec.afterAll();
 	return {total: totalRan, fail: failures};
 }
-function expect(one, context){
+expect = function expect(one, context){
     return {
         toEqual: function(other){
             if ((one && one.constructor === Date) && 
@@ -112,7 +134,7 @@ function expect(one, context){
     };
 }
 
-function fail(reason){
+fail = function fail(reason){
     throw new Error(reason || 'Failed');
 };
 describe.output = function(msg){
